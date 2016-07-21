3b6ed586891beb00b29cd84f164bae4c924f26bc
lawlite19 <849451478@qq.com>
2016-07-21 18:45:56 +0800

    修改

--- 一、排序算法/1.交换排序/冒泡排序.cpp
+++ 一、排序算法/1.交换排序/冒泡排序.cpp
@@ -5,7 +5,7 @@ using namespace std;
 void BubbleSort(int *a,int n)
 {
 	int i,j,temp;
-	for(i=0;i<n;i++){
+	for(i=0;i<n-1;i++){// n个数排序需要比较n-1趟（0~n-2），当然i<n也是没错，因为当i=n-1时，下面j<n-(n-1)-1=0,不进入下一层for循环 
 		for(j=0;j<n-i-1;j++){//因为后面比较的是a[j]和a[j+1],j会+1，所以是j<n-i-1 
 			if(a[j]>a[j+1]){
 				temp=a[j];
